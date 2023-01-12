bb0454baab3eba89e424d648e1d9d84f3a0ef4ec
Mohammad Mustakim Ali <mustakimali@users.noreply.github.com>
2023-01-12 16:40:26 +0000

    fix: /build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -eu pipefail
 
-mkdir static && cd frontend && npm -i g yarn && yarn install && yarn build && cd ..
+mkdir - p static && cd frontend && npm -i g yarn && yarn install && yarn build && cd ..
 echo
 echo "First time build complete, please run 'cargo run' to start the server"
 echo "The application will be availabe in 'http://localhost:8080'"
