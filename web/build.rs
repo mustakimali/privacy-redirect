0164058481667eb2b400b320ec6db42c850f5f9e
Mohammad Mustakim Ali <mustakimali@users.noreply.github.com>
2023-01-14 19:18:32 +0000

    lint

--- web/build.rs
+++ web/build.rs
@@ -2,10 +2,7 @@ use std::{fs, path::Path};
 
 fn main() {
     if Path::new("../frontend").exists() {
-        fs::copy(
-            "../browser-ext/script.js",
-            "../frontend/script.js",
-        )
-        .expect("copy script file from `browser-ext` folder to frontend folder");
+        fs::copy("../browser-ext/script.js", "../frontend/script.js")
+            .expect("copy script file from `browser-ext` folder to frontend folder");
     }
 }
