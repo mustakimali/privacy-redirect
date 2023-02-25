d4458b6b13e23124ae87b45c490de5876b7e0749
Mohammad Mustakim Ali <mustakimali@users.noreply.github.com>
2023-02-25 17:15:19 +0000

    now

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -eu pipefail
 
+sudo apt-get update -y && sudo apt-get install protobuf-compiler || true
+
 mkdir -p static && cd frontend && npm -i g yarn && yarn install && yarn build && cd ..
 echo
 echo "First time build complete, please run 'cargo run' to start the server"
